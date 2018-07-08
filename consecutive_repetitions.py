9a308dbed787643d913f8bd028137a33f31f742e
preeti saraswat <preetisaraswat17@gmail.com>
2018-07-07 20:41:24 -0700

    Create consecutive_repetitions.py

---
+++ consecutive_repetitions.py
@@ -0,0 +1,23 @@
+#Task
+#You are given a string . 
+#Your task is to find the first occurrence of an alphanumeric character in  (read from left to right) that has consecutive repetitions.
+
+#Input Format:
+#A single line of input containing the string .
+
+#Output Format:
+#Print the first occurrence of the repeating character. If there are no repeating characters, print -1.
+
+#Sample Input
+#..12345678910111213141516171820212223
+
+#Sample Output
+#1
+
+--------------------------------------------------------Solution-------------------------------------------------------------------
+
+import re
+pattern=r'(\w)\1+' #(alphanumeric)repetation(\1)
+x=input()
+m = re.findall(pattern,x)
+print(m[0])
